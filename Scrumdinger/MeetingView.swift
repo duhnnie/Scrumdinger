cb70543354d222338038a023ea3b1f37c7f12177
Daniel <dnl.canedo@gmail.com>
2022-01-27 23:39:11 -0400

    Section 2: Build Groups of Views - Steps 6 - 10

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -2,7 +2,19 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        ProgressView(value: 5, total: 15)
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                VStack {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+        }
     }
 }
 
